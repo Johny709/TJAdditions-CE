bad93d3bc00dc3f6c2f040ac7a895e918e5a854a
Johny709 <86203059+Johny709@users.noreply.github.com>
2026-01-02 17:05:27 -0500

    add Modular Armor logic

--- src/main/java/tj/items/behaviours/ModularArmorBehaviour.java
+++ src/main/java/tj/items/behaviours/ModularArmorBehaviour.java
@@ -2,7 +2,12 @@ package tj.items.behaviours;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import gregtech.api.GTValues;
+import gregtech.api.capability.GregtechCapabilities;
+import gregtech.api.capability.IElectricItem;
+import gregtech.api.items.armor.ArmorMetaItem;
 import gregtech.api.items.armor.ISpecialArmorLogic;
+import gregtech.api.items.metaitem.ElectricStats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -24,17 +29,25 @@ public class ModularArmorBehaviour implements ISpecialArmorLogic {
 
     @Override
     public ISpecialArmor.ArmorProperties getProperties(EntityLivingBase entityLivingBase, @Nonnull ItemStack itemStack, DamageSource damageSource, double v, EntityEquipmentSlot entityEquipmentSlot) {
-        return null;
+        return new ISpecialArmor.ArmorProperties(0, 0, Integer.MAX_VALUE);
     }
 
     @Override
     public int getArmorDisplay(EntityPlayer entityPlayer, @Nonnull ItemStack itemStack, int i) {
-        return 2;
+        IElectricItem item = itemStack.getCapability(GregtechCapabilities.CAPABILITY_ELECTRIC_ITEM, null);
+        if (item != null && item.getCharge() > 0)
+            return 6;
+        return 3;
     }
 
     @Override
     public EntityEquipmentSlot getEquipmentSlot(ItemStack itemStack) {
         return this.equipmentSlot;
+    }
+
+    @Override
+    public void addToolComponents(ArmorMetaItem.ArmorMetaValueItem metaValueItem) {
+        metaValueItem.addStats(ElectricStats.createElectricItem(1000000L, GTValues.LV));
     }
 
     @Override
@@ -49,6 +62,9 @@ public class ModularArmorBehaviour implements ISpecialArmorLogic {
 
     @Override
     public String getArmorTexture(ItemStack itemStack, Entity entity, EntityEquipmentSlot slot, String s) {
+        IElectricItem item = itemStack.getCapability(GregtechCapabilities.CAPABILITY_ELECTRIC_ITEM, null);
+        if (item != null && item.getCharge() > 0)
+            return slot == EntityEquipmentSlot.HEAD || slot == EntityEquipmentSlot.CHEST ? "gregtech:textures/armor/e2_helmet_chest.png" : "gregtech:textures/armor/e2_leggings_boots.png";
         return slot == EntityEquipmentSlot.HEAD || slot == EntityEquipmentSlot.CHEST ? "gregtech:textures/armor/e1_helmet_chest.png" : "gregtech:textures/armor/e1_leggings_boots.png";
     }
 }
