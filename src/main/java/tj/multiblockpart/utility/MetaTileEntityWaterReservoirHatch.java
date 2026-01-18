5d1221420f4dbcb649f0a544f5e788ed00415bcb
Johny709 <86203059+Johny709@users.noreply.github.com>
2026-01-18 15:14:02 -0500

    add GUI to Water Reservoir Hatch and increase throughput

--- src/main/java/tj/multiblockpart/utility/MetaTileEntityWaterReservoirHatch.java
+++ src/main/java/tj/multiblockpart/utility/MetaTileEntityWaterReservoirHatch.java
@@ -5,7 +5,9 @@ import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
 import gregicadditions.client.ClientHandler;
 import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.widgets.ProgressWidget;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.multiblock.IMultiblockAbilityPart;
@@ -14,44 +16,64 @@ import gregtech.api.unification.material.Materials;
 import gregtech.common.metatileentities.electric.multiblockpart.MetaTileEntityMultiblockPart;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import tj.gui.TJGuiTextures;
+import tj.gui.widgets.TJLabelWidget;
+import tj.gui.widgets.TJProgressBarWidget;
+import tj.util.TJFluidUtils;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class MetaTileEntityWaterReservoirHatch extends MetaTileEntityMultiblockPart implements IMultiblockAbilityPart<IFluidTank> {
 
-    private final FluidTank fluidTank;
+    private final FluidTank fluidTank = new FluidTank(Materials.Water.getFluid(Integer.MAX_VALUE), Integer.MAX_VALUE) {
+        @Nullable
+        @Override
+        public FluidStack drainInternal(int maxDrain, boolean doDrain) {
+            return new FluidStack(this.fluid, maxDrain);
+        }
+    };
 
     public MetaTileEntityWaterReservoirHatch(ResourceLocation metaTileEntityId, int tier) {
         super(metaTileEntityId, tier);
-        this.fluidTank = new FluidTank(Integer.MAX_VALUE);
-        initializeInventory();
+        this.initializeInventory();
     }
 
     @Override
     public MetaTileEntity createMetaTileEntity(MetaTileEntityHolder metaTileEntityHolder) {
-        return new MetaTileEntityWaterReservoirHatch(metaTileEntityId, getTier());
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        if (getWorld().isRemote)
-            return;
-        fluidTank.fill(Materials.Water.getFluid(Integer.MAX_VALUE), true);
+        return new MetaTileEntityWaterReservoirHatch(this.metaTileEntityId, this.getTier());
     }
 
     @Override
     protected FluidTankList createImportFluidHandler() {
-        return new FluidTankList(false, fluidTank);
+        return new FluidTankList(false, this.fluidTank);
     }
 
     @Override
-    protected ModularUI createUI(EntityPlayer entityPlayer) {
-        return null;
+    protected ModularUI createUI(EntityPlayer player) {
+        return ModularUI.defaultBuilder()
+                .widget(new TJLabelWidget(7, -18, 166, 20, TJGuiTextures.MACHINE_LABEL)
+                        .setItemLabel(this.getStackForm()).setLocale(this.getMetaFullName()))
+                .widget(new TJProgressBarWidget(4, 4, 168, 80, this::getWaterAmount, this::getWaterCapacity, true, ProgressWidget.MoveType.VERTICAL)
+                        .setLocale("tj.multiblock.bars.fluid", () -> new Object[]{this.fluidTank.getFluid().getLocalizedName()})
+                        .setTexture(GuiTextures.FLUID_SLOT)
+                        .setFluid(this.fluidTank::getFluid)
+                        .setInverted(true))
+                .bindPlayerInventory(player.inventory)
+                .build(this.getHolder(), player);
+    }
+
+    private long getWaterAmount() {
+        return TJFluidUtils.getFluidAmountFromTanks(this.fluidTank.getFluid(), this.getImportFluids());
+    }
+
+    private long getWaterCapacity() {
+        return TJFluidUtils.getFluidCapacityFromTanks(this.fluidTank.getFluid(), this.getImportFluids());
     }
 
     @Override
