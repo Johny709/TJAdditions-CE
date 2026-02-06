0ea55d2d37bce2926802f9e1bc59135dfed78f8a
Johny709 <janjoseph.merida@gmail.com>
2026-02-06 07:57:10 -0500

    add recipes to higher tier AE2 storage cells

--- src/main/java/tj/recipes/RecipeInit.java
+++ src/main/java/tj/recipes/RecipeInit.java
@@ -1,8 +1,13 @@
 package tj.recipes;
 
+import appeng.api.definitions.IItems;
+import appeng.api.definitions.IMaterials;
+import appeng.core.Api;
+import appeng.core.api.definitions.ApiBlocks;
 import gregicadditions.GAEnums;
 import gregicadditions.GAValues;
 import gregicadditions.item.GAMetaBlocks;
+import gregicadditions.item.GAMetaItems;
 import gregicadditions.item.GATransparentCasing;
 import gregicadditions.item.fusion.GAFusionCasing;
 import gregicadditions.item.metal.MetalCasing1;
@@ -27,6 +32,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import tj.TJConfig;
 import tj.blocks.*;
+import tj.integration.appeng.IApiItems;
+import tj.integration.appeng.IApiMaterials;
 import tj.machines.TJMetaTileEntities;
 import tj.recipes.ct.*;
 
@@ -68,6 +75,13 @@ public class RecipeInit {
         }
     }
     private static void craftingRecipes() {
+        ApiBlocks aeBlocks = Api.INSTANCE.definitions().blocks();
+        IApiItems aeItems = ((IApiItems) (IItems) Api.INSTANCE.definitions().items());
+        IApiMaterials aeMaterials = ((IApiMaterials) (IMaterials) Api.INSTANCE.definitions().materials());
+        ItemStack[] aeCellParts = {aeMaterials.getCell65mPart().maybeStack(1).get(), aeMaterials.getCell262mPart().maybeStack(1).get(), aeMaterials.getCell1048mPart().maybeStack(1).get(), aeMaterials.getCellDigitalSingularityPart().maybeStack(1).get(),
+            aeMaterials.getFluidCell65mPart().maybeStack(1).get(), aeMaterials.getFluidCell262mPart().maybeStack(1).get(), aeMaterials.getFluidCell1048mPart().maybeStack(1).get(), aeMaterials.getFluidCellDigitalSingularityPart().maybeStack(1).get()};
+        ItemStack[] aeCells = {aeItems.getCell65m().maybeStack(1).get(), aeItems.getCell262m().maybeStack(1).get(), aeItems.getCell1048m().maybeStack(1).get(), aeItems.getCellDigitalSingularity().maybeStack(1).get(),
+            aeItems.getFluidCell65m().maybeStack(1).get(), aeItems.getFluidCell262m().maybeStack(1).get(), aeItems.getFluidCell1048m().maybeStack(1).get(), aeItems.getFluidCellDigitalSingularity().maybeStack(1).get()};
 
         ALLOY_SMELTER_RECIPES.recipeBuilder()
                 .input(Items.CLAY_BALL)
@@ -375,6 +389,50 @@ public class RecipeInit {
                     .outputs(UNIVERSAL_CIRCUITS[i].getStackForm())
                     .EUt(2)
                     .duration(20)
+                    .buildAndRegister();
+        }
+
+        for (int i = 0; i < aeCells.length; i++) {
+            ModHandler.addShapelessRecipe("ae_cell_simple." + aeCells[i].getTranslationKey(), aeCells[i], aeMaterials.emptyStorageCell().maybeStack(1).get(), aeCellParts[i]);
+            ModHandler.addShapedRecipe("ae_cell." + aeCells[i].getTranslationKey(), aeCells[i], "QRQ", "RCR", "III",
+                    'Q', aeBlocks.quartzGlass().maybeStack(1).get(),
+                    'I', new ItemStack(Items.IRON_INGOT),
+                    'R', new ItemStack(Items.REDSTONE),
+                    'C', aeCellParts[i]);
+        }
+        for (int i = 0; i < 2; i++) {
+            ASSEMBLER_RECIPES.recipeBuilder()
+                    .circuitMeta(1)
+                    .input(OrePrefix.circuit, CIRCUIT_TIERS[i + 9])
+                    .input(OrePrefix.gemExquisite, Amethyst, 4)
+                    .input(OrePrefix.plate, MATERIAL_TIER[0][i + 9], 4)
+                    .inputs(GAMetaItems.UNSTABLE_STAR.getStackForm(), GAMetaItems.OPTICAL_PROCESSING_CORE.getStackForm(4), GAMetaItems.OPTICAL_SOC.getStackForm(16))
+                    .outputs(i == 0 ? aeCellParts[0] : aeCellParts[4])
+                    .duration(300).EUt(GAValues.VA[9])
+                    .buildAndRegister();
+        }
+        for (int i = 0; i < 4; i++) {
+            ItemStack aeCellParts2 = i == 0 ? new ItemStack(Item.getByNameOrId("nae2:material"), 3, 4) : aeCellParts[i - 1].copy();
+            aeCellParts2.setCount(3);
+            ASSEMBLER_RECIPES.recipeBuilder()
+                    .circuitMeta(1)
+                    .input(OrePrefix.circuit, CIRCUIT_TIERS[i + 9])
+                    .input(OrePrefix.gemExquisite, Amethyst, 4)
+                    .input(OrePrefix.plate, MATERIAL_TIER[0][i + 9], 4)
+                    .inputs(aeCellParts2, GAMetaItems.OPTICAL_PROCESSING_CORE.getStackForm(4))
+                    .outputs(aeCellParts[i])
+                    .duration(300).EUt(GAValues.VA[Math.min(11, i + 9)])
+                    .buildAndRegister();
+            aeCellParts2 = i == 0 ? new ItemStack(Item.getByNameOrId("nae2:material"), 3, 8) : aeCellParts[i + 3].copy();
+            aeCellParts2.setCount(3);
+            ASSEMBLER_RECIPES.recipeBuilder()
+                    .circuitMeta(1)
+                    .input(OrePrefix.circuit, CIRCUIT_TIERS[i + 9])
+                    .input(OrePrefix.gemExquisite, Amethyst, 4)
+                    .input(OrePrefix.plate, MATERIAL_TIER[0][i + 9], 4)
+                    .inputs(aeCellParts2, GAMetaItems.OPTICAL_PROCESSING_CORE.getStackForm(4))
+                    .outputs(aeCellParts[i + 4])
+                    .duration(300).EUt(GAValues.VA[Math.min(11, i + 9)])
                     .buildAndRegister();
         }
 
