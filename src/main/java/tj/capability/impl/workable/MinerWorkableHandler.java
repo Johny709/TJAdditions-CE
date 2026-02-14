d6788c2f394d8f44aa862c0e4c75e9125a46a099
Johny709 <janjoseph.merida@gmail.com>
2026-02-14 16:53:39 -0500

    add new Miner logic

--- src/main/java/tj/capability/impl/workable/MinerWorkableHandler.java
+++ src/main/java/tj/capability/impl/workable/MinerWorkableHandler.java
@@ -1,17 +1,178 @@
 package tj.capability.impl.workable;
 
 import gregtech.api.metatileentity.MetaTileEntity;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.block.*;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 import tj.capability.AbstractWorkableHandler;
+import tj.capability.IItemFluidHandlerInfo;
 import tj.capability.IMachineHandler;
+import tj.capability.TJCapabilities;
+import tj.util.ItemStackHelper;
 
-public class MinerWorkableHandler extends AbstractWorkableHandler<IMachineHandler> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinerWorkableHandler extends AbstractWorkableHandler<IMachineHandler> implements IItemFluidHandlerInfo {
+
+    private final Object2ObjectMap<String, ItemStack> itemType = new Object2ObjectOpenHashMap<>();
+    private final BlockPos.MutableBlockPos miningPos = new BlockPos.MutableBlockPos();
+    private final List<ItemStack> itemOutputs = new ArrayList<>();
+    private final List<Chunk> chunks = new ArrayList<>();
+    private boolean initialized;
+    private Chunk currentChunk;
+    private int outputIndex;
+    private int chunkIndex;
+    private int levelY;
+
     public MinerWorkableHandler(MetaTileEntity metaTileEntity) {
         super(metaTileEntity);
     }
 
     @Override
     protected boolean startRecipe() {
-        this.metaTileEntity.getWorld().getChunk(this.metaTileEntity.getPos());
-        return super.startRecipe();
+        this.initializeChunks();
+        if (this.chunkIndex >= this.chunks.size())
+            this.chunkIndex = 0;
+        this.currentChunk = this.chunks.get(this.chunkIndex++);
+        this.levelY = this.metaTileEntity.getPos().getY();
+        this.setMaxProgress(this.levelY * 256);
+        this.energyPerTick = 512;
+        return true;
+    }
+
+    @Override
+    protected void progressRecipe(int progress) {
+        super.progressRecipe(progress);
+        this.initializeChunks();
+        if (this.currentChunk == null)
+            this.currentChunk = this.chunks.get(this.chunkIndex);
+        if (this.progress > progress) {
+            progress = this.progress % 256;
+            this.miningPos.setPos(this.currentChunk.x + (progress % 16), this.levelY, this.currentChunk.z + (progress / 16));
+            IBlockState state = this.metaTileEntity.getWorld().getBlockState(this.miningPos);
+            Block block = state.getBlock();
+            if (state.getBlock() instanceof BlockDirt || state.getBlock() instanceof BlockGrass || state.getBlock() instanceof BlockStone || state.getBlock() instanceof BlockSand) {
+                Item item = block.getItemDropped(state, this.metaTileEntity.getWorld().rand, 0);
+                if (this.addItemDrop(item, 1, block.damageDropped(state)))
+                    this.metaTileEntity.getWorld().destroyBlock(this.miningPos, false);
+            }
+            if (progress == 255)
+                this.levelY--;
+        }
+    }
+
+    @Override
+    protected boolean completeRecipe() {
+        for (int i = this.outputIndex; i < this.itemOutputs.size(); i++) {
+            ItemStack stack = this.itemOutputs.get(i);
+            if (ItemStackHelper.insertIntoItemHandler(this.handler.getExportItemInventory(), stack, true).isEmpty()) {
+                ItemStackHelper.insertIntoItemHandler(this.handler.getExportItemInventory(), stack, false);
+                this.outputIndex++;
+            } else return false;
+        }
+        this.itemType.clear();
+        this.itemOutputs.clear();
+        return true;
+    }
+
+    private void initializeChunks() {
+        if (!this.initialized) {
+            this.initialized = true;
+            boolean odd = this.handler.getTier() % 2 == 0;
+            Chunk origin = this.metaTileEntity.getWorld().getChunk(this.metaTileEntity.getPos());
+            int originX = origin.x;
+            int originZ = origin.z;
+            for (int x = -this.handler.getTier() + (odd ? 1 : 0) ; x < this.handler.getTier(); x++) {
+                for (int z = -this.handler.getTier() + (odd ? 1 : 0); z < this.handler.getTier(); z++) {
+                    this.chunks.add(this.metaTileEntity.getWorld().getChunk(originX + (x * 16), originZ + (z * 16)));
+                }
+            }
+        }
+    }
+
+    private <T extends IForgeRegistryEntry<T>> boolean addItemDrop(T type, int count, int meta) {
+        if (type == null)
+            return false;
+        String key = type.getRegistryName().toString() + ":" + meta;
+        ItemStack stack = this.itemType.get(key);
+        if (stack != null) {
+            stack.grow(count);
+        } else {
+            ItemStack itemStack = type instanceof Block ? new ItemStack((Block) type, count, meta) : new ItemStack((Item) type, count, meta);
+            this.itemType.put(key, itemStack);
+            this.itemOutputs.add(itemStack);
+        }
+        return true;
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagCompound compound = super.serializeNBT();
+        NBTTagList itemOutputList = new NBTTagList();
+        for (ItemStack stack : this.itemOutputs)
+            itemOutputList.appendTag(stack.serializeNBT());
+        compound.setInteger("outputIndex", this.outputIndex);
+        compound.setInteger("chunkIndex", this.chunkIndex);
+        compound.setInteger("levelY", this.levelY);
+        compound.setInteger("x", this.miningPos.getX());
+        compound.setInteger("y", this.miningPos.getY());
+        compound.setInteger("z", this.miningPos.getZ());
+        compound.setTag("itemOutputList", itemOutputList);
+        return compound;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound compound) {
+        super.deserializeNBT(compound);
+        NBTTagList itemOutputList = compound.getTagList("itemOutputList", 10);
+        for (int i = 0; i < itemOutputList.tagCount(); i++) {
+            this.itemOutputs.add(new ItemStack(itemOutputList.getCompoundTagAt(i)));
+        }
+        this.outputIndex = compound.getInteger("outputIndex");
+        this.chunkIndex = compound.getInteger("chunkIndex");
+        this.levelY = compound.getInteger("levelY");
+        this.miningPos.setPos(compound.getInteger("x"), compound.getInteger("y"), compound.getInteger("z"));
+    }
+
+    @Override
+    public <T> T getCapability(Capability<T> capability) {
+        if (capability == TJCapabilities.CAPABILITY_ITEM_FLUID_HANDLING)
+            return TJCapabilities.CAPABILITY_ITEM_FLUID_HANDLING.cast(this);
+        return super.getCapability(capability);
+    }
+
+    @Override
+    public List<ItemStack> getItemOutputs() {
+        return this.itemOutputs;
+    }
+
+    public int getChunkIndex() {
+        return this.chunkIndex;
+    }
+
+    public int getChunkSize() {
+        return this.chunks.size();
+    }
+
+    public int getX() {
+        return this.miningPos.getX();
+    }
+
+    public int getY() {
+        return this.miningPos.getY();
+    }
+
+    public int getZ() {
+        return this.miningPos.getZ();
     }
 }
--- src/main/java/tj/machines/multi/electric/MetaTileEntityAdvancedLargeMiner.java
+++ src/main/java/tj/machines/multi/electric/MetaTileEntityAdvancedLargeMiner.java
@@ -1,5 +1,8 @@
 package tj.machines.multi.electric;
 
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
 import gregicadditions.capabilities.GregicAdditionsCapabilities;
 import gregicadditions.client.ClientHandler;
 import gregicadditions.item.GAMetaBlocks;
@@ -12,15 +15,21 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.multiblock.BlockPattern;
 import gregtech.api.multiblock.FactoryBlockPattern;
 import gregtech.api.render.ICubeRenderer;
-import gregtech.api.render.OrientedOverlayRenderer;
-import gregtech.api.render.Textures;
 import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import tj.builder.multicontrollers.TJMultiblockControllerBase;
+import tj.builder.multicontrollers.UIDisplayBuilder;
 import tj.capability.impl.workable.MinerWorkableHandler;
+import tj.textures.TJTextures;
 
-import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 import static gregtech.api.unification.material.Materials.BlackSteel;
 
@@ -39,6 +48,12 @@ public class MetaTileEntityAdvancedLargeMiner extends TJMultiblockControllerBase
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
+        tooltip.add(I18n.format("tj.multiblock.advanced_large_miner.description"));
+    }
+
+    @Override
     protected boolean shouldUpdate(MTETrait trait) {
         return false;
     }
@@ -47,6 +62,20 @@ public class MetaTileEntityAdvancedLargeMiner extends TJMultiblockControllerBase
     protected void updateFormedValid() {
         if (((this.getProblems() >> 5) & 1) != 0)
             this.workableHandler.update();
+    }
+
+    @Override
+    protected void addDisplayText(UIDisplayBuilder builder) {
+        super.addDisplayText(builder);
+        if (!this.isStructureFormed()) return;
+        builder.voltageInLine(this.getInputEnergyContainer())
+                .addTranslationLine("tj.multiblock.advanced_large_miner.chunk_index", this.workableHandler.getChunkIndex(), this.workableHandler.getChunkSize())
+                .addRecipeOutputLine(this.workableHandler, 1000);
+        if (this.workableHandler.isActive())
+            builder.addTranslationLine("metaitem.linking.device.x", this.workableHandler.getX())
+                    .addTranslationLine("metaitem.linking.device.y", this.workableHandler.getY())
+                    .addTranslationLine("metaitem.linking.device.z", this.workableHandler.getZ())
+                    .isWorkingLine(this.workableHandler.isWorkingEnabled(), this.workableHandler.isActive(), this.workableHandler.getProgress(), this.workableHandler.getMaxProgress());
     }
 
     @Override
@@ -78,10 +107,21 @@ public class MetaTileEntityAdvancedLargeMiner extends TJMultiblockControllerBase
         return ClientHandler.BLACK_STEEL_CASING;
     }
 
-    @Nonnull
     @Override
-    protected OrientedOverlayRenderer getFrontOverlay() {
-        return Textures.MULTIBLOCK_WORKABLE_OVERLAY;
+    @SideOnly(Side.CLIENT)
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        TJTextures.TJ_MULTIBLOCK_WORKABLE_OVERLAY.render(renderState, translation, pipeline, this.getFrontFacing(), this.workableHandler.isActive(), this.workableHandler.hasProblem(), this.workableHandler.isWorkingEnabled());
+    }
+
+    @Override
+    public void setWorkingEnabled(boolean isActivationAllowed) {
+        this.workableHandler.setWorkingEnabled(isActivationAllowed);
+    }
+
+    @Override
+    public boolean isWorkingEnabled() {
+        return this.workableHandler.isWorkingEnabled();
     }
 
     @Override
