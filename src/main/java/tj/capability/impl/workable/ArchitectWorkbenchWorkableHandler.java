93b02602378bd59488814f10c44eeaca06a16ce6
Johny709 <janjoseph.merida@gmail.com>
2026-04-27 17:40:01 -0400

    refactor ArchitectWorkbenchWorkableHandler

--- src/main/java/tj/capability/impl/workable/ArchitectWorkbenchWorkableHandler.java
+++ src/main/java/tj/capability/impl/workable/ArchitectWorkbenchWorkableHandler.java
@@ -4,6 +4,7 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import tj.capability.IItemFluidHandlerInfo;
@@ -13,20 +14,18 @@ import tj.capability.AbstractWorkableHandler;
 import tj.util.TJItemUtils;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<IMachineHandler> implements IItemFluidHandlerInfo {
 
+    private final List<ItemStack> itemInputs = new ArrayList<>();
+    private final List<ItemStack> itemOutputs = new ArrayList<>();
+
     @Nonnull
     private ItemStack catalyst = ItemStack.EMPTY;
-
-    @Nonnull
-    private ItemStack input = ItemStack.EMPTY;
-
-    @Nonnull
-    private ItemStack output = ItemStack.EMPTY;
+    private int outputIndex;
 
     public ArchitectWorkbenchWorkableHandler(MetaTileEntity metaTileEntity) {
         super(metaTileEntity);
@@ -58,11 +57,6 @@ public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<I
             foundRecipe = this.findCatalyst(itemInputs) && this.findInputs(itemInputs);
         }
         if (foundRecipe) {
-            this.output = new ItemStack(Item.getByNameOrId("architecturecraft:shape"), this.input.getCount());
-            NBTTagCompound compound = this.catalyst.getTagCompound().copy();
-            compound.setString("BaseName", Item.REGISTRY.getNameForObject(this.input.getItem()).toString());
-            compound.setInteger("BaseData", this.input.getMetadata());
-            this.output.setTagCompound(compound);
             this.maxProgress = this.calculateOverclock(30, 200, 2.8F);
             return true;
         }
@@ -71,19 +65,23 @@ public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<I
 
     @Override
     protected boolean completeRecipe() {
-        if (TJItemUtils.insertIntoItemHandler(this.handler.getExportItemInventory(), this.output, true).isEmpty()) {
-            TJItemUtils.insertIntoItemHandler(this.handler.getExportItemInventory(), this.output, false);
-            this.catalyst = ItemStack.EMPTY;
-            this.input = ItemStack.EMPTY;
-            this.output = ItemStack.EMPTY;
-            return true;
+        for (int i = this.outputIndex; i < this.itemOutputs.size(); i++) {
+            final ItemStack stack = this.itemOutputs.get(i);
+            if (TJItemUtils.insertIntoItemHandler(this.handler.getExportItemInventory(), stack, true).isEmpty()) {
+                TJItemUtils.insertIntoItemHandler(this.handler.getExportItemInventory(), stack, false);
+                this.outputIndex++;
+            } else return false;
         }
-        return false;
+        this.outputIndex = 0;
+        this.itemInputs.clear();
+        this.itemOutputs.clear();
+        this.catalyst = ItemStack.EMPTY;
+        return true;
     }
 
     private boolean findCatalyst(IItemHandlerModifiable itemInputs) {
         for (int i = 0; i < itemInputs.getSlots(); i++) {
-            ItemStack stack = itemInputs.getStackInSlot(i);
+            final ItemStack stack = itemInputs.getStackInSlot(i);
             if (this.isArchitectureStack(stack.getTagCompound())) {
                 this.catalyst = stack;
                 return true;
@@ -94,24 +92,24 @@ public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<I
 
     private boolean findInputs(IItemHandlerModifiable itemInputs) {
         int availableParallels = this.handler.getParallel();
-        int count = 0;
-        for (int i = 0; i < itemInputs.getSlots() && availableParallels != 0; i++) {
-            ItemStack stack = itemInputs.getStackInSlot(i);
+        for (int i = 0; i < itemInputs.getSlots() && availableParallels > 0; i++) {
+            final ItemStack stack = itemInputs.getStackInSlot(i);
             if (stack.isEmpty() || this.isArchitectureStack(stack.getTagCompound()))
                 continue;
-            if (this.input.isEmpty())
-                this.input = stack.copy();
-            if (!stack.isItemEqual(this.input))
-                continue;
-            int reminder = Math.min(stack.getCount(), availableParallels);
-            if (this.handler.getImportItemInventory().extractItem(i, reminder, true).getCount() == reminder) {
-                this.handler.getImportItemInventory().extractItem(i, reminder, false);
-                availableParallels -= reminder;
-                count += reminder;
-                this.input.setCount(count);
+            if (!this.handler.getImportItemInventory().extractItem(i, availableParallels, true).isEmpty()) {
+                final ItemStack input = this.handler.getImportItemInventory().extractItem(i, availableParallels, false);
+                final int inputCount = input.getCount();
+                final ItemStack output = new ItemStack(Item.getByNameOrId("architecturecraft:shape"), inputCount);
+                final NBTTagCompound compound = this.catalyst.getTagCompound().copy();
+                compound.setString("BaseName", Item.REGISTRY.getNameForObject(input.getItem()).toString());
+                compound.setInteger("BaseData", input.getMetadata());
+                output.setTagCompound(compound);
+                availableParallels -= inputCount;
+                this.itemInputs.add(input);
+                this.itemOutputs.add(output);
             }
         }
-        return count > 0;
+        return availableParallels != this.handler.getParallel();
     }
 
     private boolean isArchitectureStack(NBTTagCompound tagCompound) {
@@ -120,25 +118,31 @@ public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<I
 
     @Override
     public NBTTagCompound serializeNBT() {
-        NBTTagCompound compound = super.serializeNBT();
+        final NBTTagCompound compound = super.serializeNBT();
+        final NBTTagList itemInputList = new NBTTagList(), itemOutputList = new NBTTagList();
         if (!this.catalyst.isEmpty())
             compound.setTag("catalyst", this.catalyst.serializeNBT());
-        if (!this.input.isEmpty())
-            compound.setTag("input", this.input.serializeNBT());
-        if (!this.output.isEmpty())
-            compound.setTag("output", this.output.serializeNBT());
+        for (ItemStack stack : this.itemInputs)
+            itemInputList.appendTag(stack.serializeNBT());
+        for (ItemStack stack : this.itemOutputs)
+            itemOutputList.appendTag(stack.serializeNBT());
+        compound.setTag("itemInputs", itemInputList);
+        compound.setTag("itemOutputs", itemOutputList);
+        compound.setInteger("outputIndex", this.outputIndex);
         return compound;
     }
 
     @Override
     public void deserializeNBT(NBTTagCompound compound) {
         super.deserializeNBT(compound);
+        final NBTTagList itemInputList = compound.getTagList("itemInputs", 10), itemOutputsList = compound.getTagList("itemOutputs", 10);
         if (compound.hasKey("catalyst"))
             this.catalyst = new ItemStack(compound.getCompoundTag("catalyst"));
-        if (compound.hasKey("input"))
-            this.input = new ItemStack(compound.getCompoundTag("input"));
-        if (compound.hasKey("output"))
-            this.output = new ItemStack(compound.getCompoundTag("output"));
+        this.outputIndex = compound.getInteger("outputIndex");
+        for (int i = 0; i < itemInputList.tagCount(); i++)
+            this.itemInputs.add(new ItemStack(itemInputList.getCompoundTagAt(i)));
+        for (int i = 0; i < itemOutputsList.tagCount(); i++)
+            this.itemOutputs.add(new ItemStack(itemOutputsList.getCompoundTagAt(i)));
     }
 
     @Override
@@ -150,11 +154,11 @@ public class ArchitectWorkbenchWorkableHandler extends AbstractWorkableHandler<I
 
     @Override
     public List<ItemStack> getItemInputs() {
-        return !this.input.isEmpty() ? Collections.singletonList(this.input) : null;
+        return this.itemInputs;
     }
 
     @Override
     public List<ItemStack> getItemOutputs() {
-        return !this.output.isEmpty() ? Collections.singletonList(this.output) : null;
+        return this.itemOutputs;
     }
 }
