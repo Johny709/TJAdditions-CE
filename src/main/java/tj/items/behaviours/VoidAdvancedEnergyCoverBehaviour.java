a244da32983d8cbfaab8aba9d07cbc97f1879d9d
Johny709 <janjoseph.merida@gmail.com>
2026-05-22 21:18:45 -0400

    fix Advanced Energy Cover missing power button and adjustable tick rate

--- src/main/java/tj/items/behaviours/VoidAdvancedEnergyCoverBehaviour.java
+++ src/main/java/tj/items/behaviours/VoidAdvancedEnergyCoverBehaviour.java
@@ -21,6 +21,8 @@ import tj.gui.widgets.NewTextFieldWidget;
 import tj.gui.widgets.TJLabelWidget;
 import tj.items.TJMetaItems;
 import tj.items.covers.VoidMode;
+import tj.util.references.BooleanReference;
+import tj.util.references.IntegerReference;
 import tj.util.references.LongReference;
 import tj.util.references.ObjectReference;
 
@@ -45,14 +47,26 @@ public class VoidAdvancedEnergyCoverBehaviour extends VoidEnergyCoverBehaviour i
         final ItemStack itemStack = player.getHeldItemMainhand();
         final NBTTagCompound compound = itemStack.getOrCreateSubCompound("voidFilter");
         final ObjectReference<VoidMode> voidMode = new ObjectReference<>(VoidMode.NORMAL);
+        final IntegerReference ticks = new IntegerReference(1);
         final LongReference throughput = new LongReference();
+        final BooleanReference isWorking = new BooleanReference();
+        final BiConsumer<String, String> setTicks = (text, id) -> {
+            ticks.setValue((int) Math.max(1, Math.min(Integer.MAX_VALUE, Long.parseLong(text))));
+            compound.setInteger("ticks", ticks.getValue());
+        };
         final BiConsumer<String, String> setThroughput = (text, id) -> {
             throughput.setValue((long) Math.max(0, Math.min(Long.MAX_VALUE, Double.parseDouble(text))));
             compound.setLong("throughput", throughput.getValue());
         };
         return ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 176, 105 + 82)
                 .widget(new TJLabelWidget(7, -18, 162, 18, TJGuiTextures.MACHINE_LABEL_2)
-                        .setItemLabel(TJMetaItems.VOID_ADVANCED_ENERGY_COVER.getStackForm()).setLocale("metaitem.void_energy_cover.name"))
+                        .setItemLabel(TJMetaItems.VOID_ADVANCED_ENERGY_COVER.getStackForm()).setLocale("void_advanced_energy_cover.name"))
+                .widget(new NewTextFieldWidget<>(26, 7, 124, 18, true, ticks::toString, setTicks)
+                        .setValidator(str -> Pattern.compile("\\*?[0-9_]*\\*?").matcher(str).matches())
+                        .setTooltipText("machine.universal.ticks.operation")
+                        .setUpdateOnTyping(true))
+                .widget(new ToggleButtonWidget(7, 7, 18, 18, MINUS_BUTTON, () -> false, data -> setTicks.accept(String.valueOf((long) ticks.getValue() / 2), "")))
+                .widget(new ToggleButtonWidget(151, 7, 18, 18, PLUS_BUTTON, () -> false, data -> setTicks.accept(String.valueOf((long) ticks.getValue() * 2), "")))
                 .widget(new ToggleButtonWidget(7, 30, 18, 18, MINUS_BUTTON, () -> false, b -> setThroughput.accept(String.valueOf((double) throughput.getValue() / 2), "")))
                 .widget(new ToggleButtonWidget(151, 30, 18, 18, PLUS_BUTTON, () -> false, b -> setThroughput.accept(String.valueOf((double) throughput.getValue() * 2), "")))
                 .widget(new NewTextFieldWidget<>(26, 30, 124, 18, true, throughput::toString, setThroughput)
@@ -61,14 +75,20 @@ public class VoidAdvancedEnergyCoverBehaviour extends VoidEnergyCoverBehaviour i
                 .widget(new CycleButtonWidget(26, 50, 124, 18, VoidMode.class, voidMode::getValue, value -> {
                     compound.setInteger("voidMode", value.ordinal());
                     voidMode.setValue(value);
-                })).widget(new TJLabelWidget(7, -18, 162, 18, TJGuiTextures.MACHINE_LABEL_2)
-                        .setLocale("metaitem.void_energy_cover.name"))
+                })).widget(new ToggleButtonWidget(151, 106, 18, 18, TJGuiTextures.POWER_BUTTON, isWorking::isValue, working -> {
+                    isWorking.setValue(working);
+                    compound.setBoolean("isWorking", isWorking.isValue());
+                }).setTooltipText("machine.universal.toggle.run.mode"))
                 .widget(TJGuiUtils.bindPlayerInventory(new WidgetGroup(), player.inventory, 7, 105, itemStack))
                 .bindOpenListener(()-> {
-                    if (compound.hasKey("throughput"))
-                        throughput.setValue(compound.getLong("throughput"));
                     if (compound.hasKey("voidMode"))
                         voidMode.setValue(VoidMode.values()[compound.getInteger("voidMode")]);
+                    if (compound.hasKey("ticks"))
+                        ticks.setValue(compound.getInteger("ticks"));
+                    if (compound.hasKey("throughput"))
+                        throughput.setValue(compound.getLong("throughput"));
+                    if (compound.hasKey("isWorking"))
+                        isWorking.setValue(compound.getBoolean("isWorking"));
                 }).bindCloseListener(() -> itemStack.getOrCreateSubCompound("voidFilter").merge(compound))
                 .build(holder, player);
     }
--- src/main/java/tj/items/covers/VoidAdvancedEnergyCover.java
+++ src/main/java/tj/items/covers/VoidAdvancedEnergyCover.java
@@ -27,7 +27,9 @@ import static tj.gui.TJGuiTextures.PLUS_BUTTON;
 public class VoidAdvancedEnergyCover extends VoidEnergyCover {
 
     private VoidMode voidMode = VoidMode.NORMAL;
+    private boolean isWorking;
     private long throughput;
+    private int ticks = 1;
 
     public VoidAdvancedEnergyCover(ICoverable coverHolder, EnumFacing attachedSide) {
         super(coverHolder, attachedSide);
@@ -51,37 +53,53 @@ public class VoidAdvancedEnergyCover extends VoidEnergyCover {
     @Override
     public void onAttached(ItemStack itemStack) {
         final NBTTagCompound compound = itemStack.getOrCreateSubCompound("voidFilter");
-        if (compound.hasKey("throughput"))
-            this.throughput = compound.getLong("throughput");
         if (compound.hasKey("voidMode"))
             this.voidMode = VoidMode.values()[compound.getInteger("voidMode")];
+        if (compound.hasKey("ticks"))
+            this.ticks = compound.getInteger("ticks");
+        if (compound.hasKey("throughput"))
+            this.throughput = compound.getLong("throughput");
+        if (compound.hasKey("isWorking"))
+            this.isWorking = compound.getBoolean("isWorking");
     }
 
     @Override
     public ModularUI createUI(EntityPlayer player) {
-        return ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 176, 105 + 82)
+        return ModularUI.builder(GuiTextures.BORDERED_BACKGROUND, 176, 187)
+                .widget(new TJLabelWidget(7, -18, 162, 18, TJGuiTextures.MACHINE_LABEL_2)
+                        .setItemLabel(this.getPickItem()).setItemLabel(this.getPickItem()).setLocale("void_advanced_energy_cover.name"))
+                .widget(new NewTextFieldWidget<>(26, 7, 124, 18, true, () -> String.valueOf(this.ticks), this::setTicks)
+                        .setValidator(str -> Pattern.compile("\\*?[0-9_]*\\*?").matcher(str).matches())
+                        .setTooltipText("machine.universal.ticks.operation")
+                        .setUpdateOnTyping(true))
+                .widget(new ToggleButtonWidget(7, 7, 18, 18, MINUS_BUTTON, () -> false, data -> this.setTicks(String.valueOf((long) this.ticks / 2), "")))
+                .widget(new ToggleButtonWidget(151, 7, 18, 18, PLUS_BUTTON, () -> false, data -> this.setTicks(String.valueOf((long) this.ticks * 2), "")))
                 .widget(new ToggleButtonWidget(7, 30, 18, 18, MINUS_BUTTON, () -> false, b -> this.setThroughput(String.valueOf((double) this.throughput / 2), "")))
                 .widget(new ToggleButtonWidget(151, 30, 18, 18, PLUS_BUTTON, () -> false, b -> this.setThroughput(String.valueOf((double) this.throughput * 2), "")))
                 .widget(new NewTextFieldWidget<>(26, 30, 124, 18, true, () -> String.valueOf(this.throughput), this::setThroughput)
                         .setValidator(str -> Pattern.compile("-*?[0-9_]*\\*?").matcher(str).matches())
                         .setUpdateOnTyping(true))
                 .widget(new CycleButtonWidget(26, 50, 124, 18, VoidMode.class, () -> this.voidMode, this::setVoidMode))
-                .widget(new TJLabelWidget(7, -18, 162, 18, TJGuiTextures.MACHINE_LABEL_2)
-                        .setItemLabel(this.getPickItem()).setLocale("metaitem.void_energy_cover.name"))
+                .widget(new ToggleButtonWidget(151, 106, 18, 18, TJGuiTextures.POWER_BUTTON, () -> this.isWorking, this::setWorking)
+                        .setTooltipText("machine.universal.toggle.run.mode"))
                 .bindPlayerInventory(player.inventory, GuiTextures.SLOT, 7, 105)
                 .build(this, player);
     }
 
     @Override
     public void update() {
-        switch (this.voidMode) {
-            case NORMAL: this.energyContainer.removeEnergy(Long.MAX_VALUE);
-                break;
-            case SUPPLY: this.energyContainer.removeEnergy(this.throughput);
-                break;
-            case EXACT:
-                if (this.energyContainer.getEnergyStored() > this.throughput)
-                    this.energyContainer.removeEnergy(this.energyContainer.getEnergyStored() - this.throughput);
+        if (this.isWorking && this.coverHolder.getOffsetTimer() % this.ticks == 0) {
+            switch (this.voidMode) {
+                case NORMAL:
+                    this.energyContainer.removeEnergy(Long.MAX_VALUE);
+                    break;
+                case SUPPLY:
+                    this.energyContainer.removeEnergy(this.throughput);
+                    break;
+                case EXACT:
+                    if (this.energyContainer.getEnergyStored() > this.throughput)
+                        this.energyContainer.removeEnergy(this.energyContainer.getEnergyStored() - this.throughput);
+            }
         }
     }
 
@@ -89,14 +107,28 @@ public class VoidAdvancedEnergyCover extends VoidEnergyCover {
     public void writeToNBT(NBTTagCompound tagCompound) {
         super.writeToNBT(tagCompound);
         tagCompound.setInteger("voidMode", this.voidMode.ordinal());
+        tagCompound.setInteger("ticks", this.ticks);
         tagCompound.setLong("throughput", this.throughput);
+        tagCompound.setBoolean("isWorking", this.isWorking);
     }
 
     @Override
     public void readFromNBT(NBTTagCompound tagCompound) {
         super.readFromNBT(tagCompound);
         this.voidMode = VoidMode.values()[tagCompound.getInteger("voidMode")];
+        this.ticks = Math.max(1, tagCompound.getInteger("ticks"));
         this.throughput = tagCompound.getLong("throughput");
+        this.isWorking = tagCompound.getBoolean("isWorking");
+    }
+
+    public void setWorking(boolean isWorking) {
+        this.isWorking = isWorking;
+        this.markAsDirty();
+    }
+
+    public void setTicks(String text, String id) {
+        this.ticks = (int) Math.max(1, Math.min(Integer.MAX_VALUE, Long.parseLong(text)));
+        this.markAsDirty();
     }
 
     public void setThroughput(String text, String id) {
