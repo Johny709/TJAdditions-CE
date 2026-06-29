f22008cad234845414a16f720656fd73bc082e8a
Johny709 <janjoseph.merida@gmail.com>
2026-06-29 16:14:21 -0400

    v2.6.0

--- src/main/java/tj/TJ.java
+++ src/main/java/tj/TJ.java
@@ -30,9 +30,7 @@ public class TJ {
 
     public static final String MODID = "tj";
     public static final String NAME = "TJ";
-    public static final String VERSION = "1.0";
-
-    public static final String MODPACK_VERSION = "1.4";
+    public static final String VERSION = "2.6.0";
 
     @SidedProxy(modId = MODID, clientSide = "tj.ClientProxy", serverSide = "tj.CommonProxy")
     public static CommonProxy proxy;
